57e1a5d8e40dd83338db9f3840112f4ac895d46f
Parth Patel <pbpatel93@gmail.com>
2025-02-11 21:16:13 -0500

    add resource name in front of attribute

--- aws-eks-networking/locals.tf
+++ aws-eks-networking/locals.tf
@@ -11,7 +11,7 @@ locals {
     "kubernetes.io/cluster/${local.kube_cluster_name}" = "shared"
     "kubernetes.io/role/internal-elb"             = "1"
   }
-  tags                        = merge(default_tags, var.additional_tags)
-  public_subnet_tags          = merge(default_public_subnet_tags, var.additional_public_subnet_tags)
-  private_subnet_tags         = merge(default_private_subnet_tags, var.additional_private_subnet_tags)
+  tags                        = merge(local.default_tags, var.additional_tags)
+  public_subnet_tags          = merge(local.default_public_subnet_tags, var.additional_public_subnet_tags)
+  private_subnet_tags         = merge(local.default_private_subnet_tags, var.additional_private_subnet_tags)
 }
