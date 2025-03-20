dfbad9eef88be928eaecc848d0df4c64b2e0d6f0
Parth Patel <pbpatel93@gmail.com>
2025-03-20 17:20:47 -0400

    replace reserved keyword

--- aws-aurora-cluster/main.tf
+++ aws-aurora-cluster/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "this" {
-  name       = "${var.environment}-${var.cluster_identifier_prefix}-subnet-group"
+  name       = "${var.environment}-${var.cluster_identifier_prefix}"
   subnet_ids = var.private_subnets
   tags       = var.tags
 }
