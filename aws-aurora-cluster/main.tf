06ec8cedab42d1a65f90661b0e8316eb6e6ae209
Parth Patel <ppatel360@gatech.edu>
2025-05-29 12:36:00 -0400

    add autoscaling for aurora

--- aws-aurora-cluster/main.tf
+++ aws-aurora-cluster/main.tf
@@ -28,3 +28,45 @@ resource "aws_rds_cluster_instance" "this" {
   publicly_accessible = false
   tags                = var.tags
 }
+
+resource "aws_appautoscaling_target" "this" {
+  count = var.autoscaling_enabled ? 1 : 0
+
+  max_capacity       = var.autoscaling_max_capacity
+  min_capacity       = var.autoscaling_min_capacity
+  resource_id        = "cluster:${aws_rds_cluster.this[0].cluster_identifier}"
+  scalable_dimension = "rds:cluster:ReadReplicaCount"
+  service_namespace  = "rds"
+
+  tags = var.tags
+
+  lifecycle {
+    ignore_changes = [
+      tags_all,
+    ]
+  }
+}
+
+resource "aws_appautoscaling_policy" "this" {
+  count = var.autoscaling_enabled ? 1 : 0
+
+  name               = var.autoscaling_policy_name
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = "cluster:${aws_rds_cluster.this[0].cluster_identifier}"
+  scalable_dimension = "rds:cluster:ReadReplicaCount"
+  service_namespace  = "rds"
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = var.predefined_metric_type
+    }
+
+    scale_in_cooldown  = var.autoscaling_scale_in_cooldown
+    scale_out_cooldown = var.autoscaling_scale_out_cooldown
+    target_value       = var.predefined_metric_type == "RDSReaderAverageCPUUtilization" ? var.autoscaling_target_cpu : var.autoscaling_target_connections
+  }
+
+  depends_on = [
+    aws_appautoscaling_target.this
+  ]
+}
--- aws-aurora-cluster/variables.tf
+++ aws-aurora-cluster/variables.tf
@@ -66,3 +66,57 @@ variable "apply_immediately" {
   type        = bool
   default     = true
 }
+
+variable "autoscaling_enabled" {
+  description = "Determines whether autoscaling of the cluster read replicas is enabled"
+  type        = bool
+  default     = false
+}
+
+variable "autoscaling_max_capacity" {
+  description = "Maximum number of read replicas permitted when autoscaling is enabled"
+  type        = number
+  default     = 2
+}
+
+variable "autoscaling_min_capacity" {
+  description = "Minimum number of read replicas permitted when autoscaling is enabled"
+  type        = number
+  default     = 0
+}
+
+variable "autoscaling_policy_name" {
+  description = "Autoscaling policy name"
+  type        = string
+  default     = "target-metric"
+}
+
+variable "predefined_metric_type" {
+  description = "The metric type to scale on. Valid values are `RDSReaderAverageCPUUtilization` and `RDSReaderAverageDatabaseConnections`"
+  type        = string
+  default     = "RDSReaderAverageCPUUtilization"
+}
+
+variable "autoscaling_scale_in_cooldown" {
+  description = "Cooldown in seconds before allowing further scaling operations after a scale in"
+  type        = number
+  default     = 300
+}
+
+variable "autoscaling_scale_out_cooldown" {
+  description = "Cooldown in seconds before allowing further scaling operations after a scale out"
+  type        = number
+  default     = 300
+}
+
+variable "autoscaling_target_cpu" {
+  description = "CPU threshold which will initiate autoscaling"
+  type        = number
+  default     = 70
+}
+
+variable "autoscaling_target_connections" {
+  description = "Average number of connections threshold which will initiate autoscaling. Default value is 70% of db.r4/r5/r6g.large's default max_connections"
+  type        = number
+  default     = 700
+}
